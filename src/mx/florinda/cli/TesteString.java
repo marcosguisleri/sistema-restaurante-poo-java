642ecc3fc9c34d8845491ef2a1ac0d2b6c0ccb47
marcosguisleri <gustavoguisleri20@gmail.com>
2026-02-03 00:24:47 -0300

    feat: adiciona classe TesteString para estudo da API String
    - Demonstra métodos length, charAt e contains
    - Uso de equals e equalsIgnoreCase
    - split, substring e replace
    - Conversão para maiúsculas e minúsculas

---
+++ src/mx/florinda/cli/TesteString.java
@@ -0,0 +1,44 @@
+package mx.florinda.cli;
+
+public class TesteString {
+
+    void main() {
+
+        String nomeItem1 = "Refresco do Chaves";
+
+        IO.println(nomeItem1.length());
+        IO.println(nomeItem1.charAt(0));
+        IO.println(nomeItem1.charAt(17));
+
+        IO.println(nomeItem1.contains("Chaves"));
+        IO.println(nomeItem1.contains("Kico"));
+        IO.println(nomeItem1.contains("chaves"));
+        IO.println("startsWith: " + nomeItem1.startsWith("Chaves"));
+        IO.println("concat: " + nomeItem1.concat(" teste"));
+        IO.println("endsWith: " + nomeItem1.endsWith("Chaves"));
+
+        String[] pedacos = nomeItem1.split(" ");
+        IO.println(pedacos.length);
+        for (String pedaco : pedacos) {
+            IO.println(pedaco);
+        }
+
+        IO.println(nomeItem1 == "Refresco do Chaves");
+        String nomeItemDigitado = IO.readln("Digite: ");
+        IO.println(nomeItem1 == nomeItemDigitado);
+        IO.println(nomeItem1.equals(nomeItemDigitado)); // case-sensitive
+        IO.println(nomeItem1.equalsIgnoreCase(nomeItemDigitado)); // case-insensitive
+
+        IO.println(nomeItem1.toUpperCase());
+        IO.println(nomeItem1.toLowerCase());
+        IO.println(nomeItem1.toLowerCase().replace(" ", "-"));
+        IO.println("substring: " + nomeItem1.substring(0, 8));
+        IO.println("substring: " + nomeItem1.substring(12));
+
+        String nomeItem1Maiusculas = nomeItem1.toUpperCase();
+
+        IO.println(nomeItem1);
+        IO.println(nomeItem1Maiusculas);
+
+    }
+}
